14e90716dcf6aeecb9b2ed314ec15dd839eb736e
Vitaliy <levchv@gmail.com>
2018-09-03 10:01:58 +0300

    added db project with product table

---
+++ ProductCatalog.Database.Sql/dbo/Tables/Product.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Code]        NVARCHAR (50)  NOT NULL,
+    [Name]        NVARCHAR (255) NOT NULL,
+    [Price]       FLOAT (53)     NOT NULL,
+    [Photo]       NVARCHAR (MAX) NULL,
+    [LastUpdated] DATETIME       NOT NULL,
+    CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([Code] ASC)
+);
+
